40c07f800f5bcc5d2e8cc4309fc54c89479ee581
Tiago Muck <tmuck@uci.edu>
2018-01-17 15:13:45 -0800

    Removed reference wrappers from sensing interface
    Let's say we have the code:
    
    const Resource *r = ...
    sense<WHATEVER>(r,wid);
    
    We want the sense call to be equivalent to:
    
    sense<WHATEVER,Resource>(r,wid)
    
    so the overloaded version of sense that takes a const Resource* is
    called. However, the second template parameter was being infered as
    const* which made the initial call equivalent to:
    
    sense<WHATEVER,const Resource*>(r,wid)
    
    which called the overload version of sense that takes a reference (in
    this case would be a reference to a pointer). This will cause either
    a "ambiguous call error" or some other error since the second call
    would be forwarding a pointer-to-a-pointer.
    
    Anyway, now only the pointer verison exists and resources are passed as
    pointer only.

--- src/runtime/framework/sensing_interface.h
+++ src/runtime/framework/sensing_interface.h
@@ -1,6 +1,8 @@
 #ifndef __arm_rt_framework_sensing_interface_h
 #define __arm_rt_framework_sensing_interface_h
 
+#include <type_traits>
+
 #include "types.h"
 #include <core/core.h>
 
@@ -28,60 +30,6 @@ class SensingInterface {
 
 	template<SensingType SEN_T,typename ResourceT>
 	static typename SensingTypeInfo<SEN_T>::ValType senseAgg(typename SensingTypeInfo<SEN_T>::ParamType p, const ResourceT *rsc, int wid);
-
-	/*
-	 * Wrappers for the functions above supporting the resource being passed as ref
-	 * or non const
-	 */
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType sense(ResourceT *rsc, int wid)
-	{ return sense<SEN_T,ResourceT>((const ResourceT*)rsc,wid); }
-
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType sense(ResourceT &rsc, int wid)
-	{ return sense<SEN_T,ResourceT>(&rsc,wid); }
-
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType sense(const ResourceT &rsc, int wid)
-	{ return sense<SEN_T,ResourceT>(&rsc,wid); }
-
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType senseAgg(ResourceT *rsc, int wid)
-	{ return senseAgg<SEN_T,ResourceT>((const ResourceT*)rsc,wid); }
-
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType senseAgg(ResourceT &rsc, int wid)
-	{ return senseAgg<SEN_T,ResourceT>(&rsc,wid); }
-
-	template<SensingType SEN_T,typename ResourceT>
-	static typename SensingTypeInfo<SEN_T>::ValType senseAgg(const ResourceT &rsc, int wid)
-	{ return senseAgg<SEN_T,ResourceT>(&rsc,wid); }
-
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType sense(typename SensingTypeInfo<SEN_T>::ParamType p, ResourceT *rsc, int wid)
-    { return sense<SEN_T,ResourceT>(p,(const ResourceT*)rsc,wid); }
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType sense(typename SensingTypeInfo<SEN_T>::ParamType p, ResourceT &rsc, int wid)
-    { return sense<SEN_T,ResourceT>(p,&rsc,wid); }
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType sense(typename SensingTypeInfo<SEN_T>::ParamType p, const ResourceT &rsc, int wid)
-    { return sense<SEN_T,ResourceT>(p,&rsc,wid); }
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType senseAgg(typename SensingTypeInfo<SEN_T>::ParamType p, ResourceT *rsc, int wid)
-    { return senseAgg<SEN_T,ResourceT>(p,(const ResourceT*)rsc,wid); }
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType senseAgg(typename SensingTypeInfo<SEN_T>::ParamType p, ResourceT &rsc, int wid)
-    { return senseAgg<SEN_T,ResourceT>(p,&rsc,wid); }
-
-    template<SensingType SEN_T,typename ResourceT>
-    static typename SensingTypeInfo<SEN_T>::ValType senseAgg(typename SensingTypeInfo<SEN_T>::ParamType p, const ResourceT &rsc, int wid)
-    { return senseAgg<SEN_T,ResourceT>(p,&rsc,wid); }
-
 };
 
 #endif /* ACTUATOR_H_ */
