f1cde06869a4d2487e2c265f9d4a9fe56f87d848
Tiago Muck <tmuck@uci.edu>
2018-01-23 15:33:22 -0800

    Updated scripts default confs

--- scripts/.confs.sh
+++ scripts/.confs.sh
@@ -25,7 +25,7 @@ export R_HOST=128.195.55.158
 export R_SPARTA_ROOT=/home/odroid/sparta_dev
 
 # Uncomment for verbose remote commands
-# export R_VERBOSE=-v
+#export R_VERBOSE=-v
 
 
 ###########################################
@@ -34,8 +34,8 @@ export R_SPARTA_ROOT=/home/odroid/sparta_dev
 # perfcnts to trace
 # (note: intrs and busyCy are always enabled when tracing,
 #        so it's not necessary to include in the list)
-TRACE_PERFCNTS=(brMisspred)
-#TRACE_PERFCNTS=(branchInstr memRdWrInstr memRdInstr memWrInstr brMisspred dtlbMiss itlbMiss l1dcAccess l1dcMiss l1icAccess l1icMiss llcAccess llcMiss busAccess busCy)
+export TRACE_PERFCNTS=(brMisspred)
+#export TRACE_PERFCNTS=(branchInstr memRdWrInstr memRdInstr memWrInstr brMisspred dtlbMiss itlbMiss l1dcAccess l1dcMiss l1icAccess l1icMiss llcAccess llcMiss busAccess busCy)
 
 # Maximum number of perfcnts to collect in a single run.
 # If the number of counters listed by TRACE_PERFCNTS is
@@ -43,15 +43,15 @@ TRACE_PERFCNTS=(brMisspred)
 # to collect all counters.
 # This number cannot be greater than the maximum counters
 # supported by the platform
-TRACE_MAXPERFCNT=3 
+export TRACE_MAXPERFCNT=3 
 
 # Number of times to run the same program.
 # Multiple runs can be combined to generate
 # a trace with the average measurements across
 # all runs
-TRACE_RUNITERS=1
+export TRACE_RUNITERS=4
 
 # Where the traces are stored
-TRACE_OUTPUT_DIR=$SPARTA_ROOT/traces
-
+export TRACE_OUTPUT_DIR=$SPARTA_ROOT/traces
+export R_TRACE_OUTPUT_DIR=$R_SPARTA_ROOT/traces
 
