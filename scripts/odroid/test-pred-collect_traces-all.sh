7cb3e5a46a9b11ce7d6d9fde37f3eae63fa8a896
Tiago Muck <tmuck@uci.edu>
2018-06-19 16:11:11 -0700

    Additional training script for odroid

---
+++ scripts/odroid/test-pred-collect_traces-all.sh
@@ -0,0 +1,45 @@
+#-------------------------------------------------------------------------------
+# Copyright (C) 2018 Tiago R. Muck <tmuck@uci.edu>
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#-------------------------------------------------------------------------------
+
+# Min
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 2 200000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 200000
+
+# Max
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 2 1400000
+sudosh $SPARTA_SCRIPTDIR/odroid/fan_max.sh # Gonna get Hot!
+sleep 5 # cooldown time
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 2000000
+sleep 5 # cooldown time
+sudosh $SPARTA_SCRIPTDIR/odroid/fan_auto.sh
+
+# Middle
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 2 800000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 1100000
+
+# Some other freqs for each
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 2 500000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 500000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 800000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 2 1100000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 1400000
+sh $SPARTA_SCRIPTDIR/training/run_training-all.sh 6 1700000
+
+
+
+
+
