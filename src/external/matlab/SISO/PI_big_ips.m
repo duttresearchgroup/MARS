c4a5dd263918c4a2be9f6c15e2fb902b2be6e9cc
Kasra Moazzemi <k.moazzemi@gmail.com>
2019-02-11 13:29:33 -0800

    Siso sample code cleaned with comments
    . This includes reading the data from excel sheet, script to design a PI
    controller and a full simulink module to evaluate a PID controller with
    a sample plant.l

--- src/external/matlab/SISO/PI_big_ips.m
+++ src/external/matlab/SISO/PI_big_ips.m
@@ -1,21 +1,27 @@
+% Sample script to design a PI controller for controlling IPS of CPU
+% cluster
+
 Mp_t = 0.1;
 ks_t = 5;
 r_t = exp(-4/ks_t);
 theta_t = pi*(log(r_t)/log(Mp_t));
 % r = 0.4, theta = 1.0
 
-%little=tf(2.1, [1 -.1834],1);
+% transfer function is extracted in system identification phase
 big=tf(3.62, [1 0.1015],1);
+
+% Discrete-time PI controller in parallel form.
 
 [C_pi,info] = pidtune(big,'PI',1)
 
-%Kp = 0.452, Ki = 0.38
+% Kp and Ki are represented at this point
 
 % control = tf([0.1 0], [1 -1], -1) + 0.05;
 PIC = feedback(big*C_pi,1);
 clpoles = pole(PIC);
 dcgain(PIC);
 
+% Demonstrating Root Locus and Step response
 [r,index] = max(abs(clpoles));
 ks = -4/log(r)
 subplot(1,2,1);
